aa609335d0afa94ef3fec8e249d1dbe4714ce914
Adeel Akhani <151796315+adeelakhani@users.noreply.github.com>
2025-06-28 19:00:59 -0400

    Delete app directory

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-# from dotenv import load_dotenv 
-# from agents import Agent, Runner, trace
-app = FastAPI()
-
-@app.get("/")
-async def hello():
-    return {"message": "Hello from uv + FastAPI!"}
